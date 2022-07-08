3d67f9d05dd16e29a070ab69aaf82275ad03e28d
liamza1 <liam.streak@gmail.com>
2022-07-08 20:00:25 +0200

    finished project submit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a6cec32c0e89e48e0b05bf142c70e27c-768927903.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
